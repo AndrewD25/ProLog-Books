cb716e377b29061de1920f56f362fbf569ed51e0
Andrew Deal <111801931+AndrewD25@users.noreply.github.com>
2024-01-27 14:09:12 -0700

    1/27
    Added more functionality to puzzle page

--- Scripts/puzzle.js
+++ Scripts/puzzle.js
@@ -6,17 +6,17 @@ DUE DATE
 
 "use strict";
 
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log('Connected to PlanetScale!')
-connection.end()
-
 // Set initial variables
+let roundText = document.getElementById("roundText");
+let roundNum = document.getElementById("roundNum");
+const guessBtn = document.getElementById("guessButton");
 let round = 1;
 let scaleFactor = [1, 2, 3, 5, 10, 25]; // Should go from 1, 2, 3, 5, 10, 25
 let sfi = 0; //index for scale factor array
 let correct = false;
+
+//Set up confetti
+const jsConfetti = new JSConfetti()
 
 // Create object with answer from database
 let correctAnswer = {
@@ -53,7 +53,7 @@ function guess() {
         let guess_p = document.createElement("p");
         guess_p.classList.add("guess");
         guess_p.innerHTML = correct ? `<i class="fa-solid fa-check green"></i> ` : `<i class="fa-solid fa-x red"></i> `;
-        guess_p.innerHTML = guess_p.innerHTML + `${guess.series} ${guess.number}`;
+        guess_p.innerHTML = guess_p.innerHTML + `${guess.series} #${guess.number}`;
         guessContainer.appendChild(guess_p);
 
         // Clear the values of the guess inputs
@@ -63,16 +63,26 @@ function guess() {
         // Increment Round
         round++;         
         sfi++;
+        if (round <= 6) {roundNum.innerHTML = round;}
         eightBit(document.getElementById('mycanvas'), img, scaleFactor[sfi]);   
     }
 
-    if (correct) {
+    if (correct || round > 6) {
+        eightBit(document.getElementById('mycanvas'), img, 100); //Make image fully visible 
+        guessBtn.onclick = null;
+        guessBtn.style.backgroundColor = "#555";
+    }
+
+    if (correct) { //Win
+        jsConfetti.addConfetti()
+        roundText.innerHTML = "You got it!"
+    } else if (round > 6) { //Lose
         eightBit(document.getElementById('mycanvas'), img, 100); //Make image fully visible
-    } else if (round > 6) {
-        eightBit(document.getElementById('mycanvas'), img, 100); //Make image fully visible
+        roundText.innerHTML = `The correct answer was... ${correctAnswer.series} #${correctAnswer.number}`
     }
 }
 
 
 // On Startup Functions to Run //
+guessBtn.onclick = guess;
 autocomplete(document.getElementById("seriesInput"));
