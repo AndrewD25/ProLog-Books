c464aacc623737c93ed3914a117f3d0a684e543f
Andrew Deal <111801931+AndrewD25@users.noreply.github.com>
2024-01-26 19:40:08 -0700

    1/26
    Testing Nodejs

--- Scripts/puzzle.js
+++ Scripts/puzzle.js
@@ -6,61 +6,34 @@ DUE DATE
 
 "use strict";
 
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+connection.end()
+
+// Set initial variables
 let round = 1;
-let scaleFactor = 10; // Set the initial scale factor to 10
-let seconds = 20;
-let timer;
+let scaleFactor = [1, 2, 3, 5, 10, 25]; // Should go from 1, 2, 3, 5, 10, 25
+let sfi = 0; //index for scale factor array
+let correct = false;
+
+// Create object with answer from database
+let correctAnswer = {
+    series: "Action Comics",
+    number: 1
+}
 
 // Run 8-bit Function for Image //
 let img = new Image();
 img.onload = function () {
-    eightBit(document.getElementById('mycanvas'), img, scaleFactor);
+    eightBit(document.getElementById('mycanvas'), img, scaleFactor[sfi]);
 };
-img.src = '../Images/exampleCover.png';
+img.src = '../Images/Puzzles/aa.jpg'; // Set based on database information from the day
 
-// Start Timer Function //
-function startTimer() {
-    timer = setInterval(updateTimer, 100); // Update every decisecond
-}
-
-// Update Timer Function //
-function updateTimer() {
-    if (seconds > 0) {
-        seconds -= 0.1; // Decrease by decisecond
-
-        // Round to one decimal place
-        seconds = Math.round(seconds * 10) / 10;
-
-        // Update the 8-bit function with the new scale factor when an integer is reached
-        if (Number.isInteger(seconds)) {
-            // Adjust the scale factor increase over time
-            scaleFactor += Math.pow(scaleFactor / 10, 1.2);
-
-            // Ensure scaleFactor stays within the range 1-100
-            scaleFactor = Math.min(Math.max(scaleFactor, 1), 100);
-
-            // Update 8-bit function with the new scale factor
-            eightBit(document.getElementById('mycanvas'), img, scaleFactor);
-        }
-
-        // Update the slider based on the current seconds
-        updateSlider(seconds);
-    } else {
-        // Stop the timer when it reaches 0
-        clearInterval(timer);
-    }
-}
-
-function updateSlider(seconds) {
-    let slider = document.getElementById('slider');
-    let position = 100 - (seconds * 5); // Adjust the factor as needed
-    slider.style.right = position + '%';
-    slider.textContent = String(Math.ceil(seconds));
-}
-updateSlider(seconds);
 
 // Guess Function //
-function guess() {
+function guess() { 
     let guessContainer = document.getElementById('guessContainer');
 
     let seriesInput = document.getElementById('seriesInput');
@@ -70,19 +43,36 @@ function guess() {
         number: numberInput.value
     };
 
-    if (round <= 6) {
+    if (round <= 6 && !correct) {
         // Guess functionality
+        if (guess.series == correctAnswer.series && guess.number == correctAnswer.number) {
+            correct = true;
+        }
 
         // Add p to guess list
         let guess_p = document.createElement("p");
         guess_p.classList.add("guess");
-        guess_p.textContent = `${guess.series} ${guess.number}`;
+        guess_p.innerHTML = correct ? `<i class="fa-solid fa-check green"></i> ` : `<i class="fa-solid fa-x red"></i> `;
+        guess_p.innerHTML = guess_p.innerHTML + `${guess.series} ${guess.number}`;
         guessContainer.appendChild(guess_p);
 
+        // Clear the values of the guess inputs
+        seriesInput.value = "";
+        numberInput.value = "";
+
         // Increment Round
-        round++;
+        round++;         
+        sfi++;
+        eightBit(document.getElementById('mycanvas'), img, scaleFactor[sfi]);   
+    }
+
+    if (correct) {
+        eightBit(document.getElementById('mycanvas'), img, 100); //Make image fully visible
+    } else if (round > 6) {
+        eightBit(document.getElementById('mycanvas'), img, 100); //Make image fully visible
     }
 }
+
 
 // On Startup Functions to Run //
 autocomplete(document.getElementById("seriesInput"));
