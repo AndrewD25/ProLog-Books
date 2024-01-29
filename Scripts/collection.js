6ba098f33a7f21f7b9eda45a006c48ef8f90173d
Andrew Deal <111801931+AndrewD25@users.noreply.github.com>
2024-01-29 14:22:03 -0700

    1/29
    Worked on collection page

--- Scripts/collection.js
+++ Scripts/collection.js
@@ -58,29 +58,7 @@ let allBooks = document.getElementsByClassName("book");
 let folderContainer = document.getElementById("folderContainer"); //This is where folder links are added
 let collectionContainer = document.getElementById("collectionContainer"); //This is where actual folders and books are added
 let allFolderLinks = document.getElementsByClassName("folderLink");
-let allFlipButtons = document.getElementsByClassName("flipButton");
 let allAddBookButtons = document.getElementsByClassName("addBookButton");
-
-//Add flip ability to all books REFACTOR INTO DRAW FUNCTION
-for (let i = 0; i < allFlipButtons.length; i++) {
-    allFlipButtons[i].addEventListener("click", function(e) {
-        // Traverse up the DOM to find the closest parent with the 'book' class
-        let currentBook = e.target.closest('.book');
-
-        if (currentBook) {
-            // Toggle the 'flipped' class on the found book
-            currentBook.classList.toggle("flipped");
-
-            //Remove 'flipped' from other books
-            let flippedBooks = document.getElementsByClassName("flipped");
-            for (let j = 0; j < flippedBooks.length; j++) {
-                if (flippedBooks[j] !== currentBook) {
-                    flippedBooks[j].classList.remove("flipped");
-                }
-            }
-        }
-    });
-}
 
 // Draw Elements of the Page on Load
 function draw() {
@@ -88,8 +66,7 @@ function draw() {
     for (let folderName in folders) {
         //Create the folder divs
         let newFolder = document.createElement("div");
-        newFolder.classList.add("folder");
-            // ⚠️ ALSO ADD THE HIDDEN CLASS TO EACH FOLDER, ADD CODE AFTER TESTING WITHOUT IT
+        newFolder.classList.add("folder", "hidden");
         newFolder.id = folderName;
         collectionContainer.appendChild(newFolder)
 
@@ -127,7 +104,17 @@ function draw() {
             let notesBox = document.createElement("textarea"); //Create notes area
             notesBox.classList.add("notes");
             detailsDiv.appendChild(notesBox);
-            // ⚠️ TO-DO: Create the elements for the read and rating selects
+            let selectBoxDiv = document.createElement("div"); //Create box for selects (read + rating)
+            selectBoxDiv.classList.add("selectBox");
+            let readSelect = document.createElement("select"); //Create the select element for read
+            readSelect.classList.add("read");
+            readSelect.innerHTML = `<option>---</option><option>Unread</option><option>Reading</option><option>Read</option>`;
+            selectBoxDiv.appendChild(readSelect);
+            let ratingSelect = document.createElement("select"); //Create the select element for rating
+            ratingSelect.classList.add("rating");
+            ratingSelect.innerHTML = `<option>---</option><option>Unhaul</option><option>Meh</option><option>Good</option><option>Great</option><option>Favorite</option>`;
+            selectBoxDiv.appendChild(ratingSelect);
+            detailsDiv.appendChild(selectBoxDiv);
             let bottomBtnDiv = document.createElement("div"); //Create div for bottom buttons
             bottomBtnDiv.classList.add("bookBottomButtons");
             let btmLeftBtn = document.createElement("button"); //Move book left btn
@@ -143,50 +130,63 @@ function draw() {
             newBook.appendChild(detailsDiv);
 
             //Add the "bookButtons" that are absolute positioned over it
-
+            let addBookBeforeBtn = document.createElement("button");
+            addBookBeforeBtn.classList.add("bookCircle", "addBookButton", "before");
+            addBookBeforeBtn.innerHTML = "+";
+            newBook.appendChild(addBookBeforeBtn);
+            let positionNumDiv = document.createElement("div");
+            positionNumDiv.classList.add("bookCircle", "positionNum");
+            positionNumDiv.innerHTML = "1";
+            newBook.appendChild(positionNumDiv);
+            let editBtn = document.createElement("div");
+            editBtn.classList.add("bookCircle", "editButton");
+            editBtn.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
+            newBook.appendChild(editBtn);
+            let copyBtn = document.createElement("button");
+            copyBtn.classList.add("bookCircle", "copyButton");
+            copyBtn.innerHTML = `<i class="fa-solid fa-copy"></i>`;
+            newBook.appendChild(copyBtn);
             let flipBtn = document.createElement("button");
             flipBtn.classList.add("bookCircle", "flipButton");
             flipBtn.innerHTML = `<i class="fa-solid fa-arrows-spin"></i>`;
+            flipBtn.addEventListener("click", function(e) { //Make Books flippable
+                let currentBook = e.target.closest('.book');
+                if (currentBook) {
+                    // Toggle the 'flipped' class on the found book
+                    currentBook.classList.toggle("flipped");
+        
+                    //Remove 'flipped' from other books
+                    let flippedBooks = document.getElementsByClassName("flipped");
+                    for (let j = 0; j < flippedBooks.length; j++) {
+                        if (flippedBooks[j] !== currentBook) {
+                            flippedBooks[j].classList.remove("flipped");
+                        }
+                    }
+                }
+            })
             newBook.appendChild(flipBtn);
-
+            let addBookAfterBtn = document.createElement("button");
+            addBookAfterBtn.classList.add("bookCircle", "addBookButton", "after");
+            addBookAfterBtn.innerHTML = "+";
+            newBook.appendChild(addBookAfterBtn);
 
             //Append the book to the folder
             newFolder.appendChild(newBook);
         });
-
-        /*
-        Temporary Documentation for me for later
-        -----------------------------------------
-         -- folderName //The name/property of the folders object
-         -- folderBooks //The array that stores all the books that go in a folder
-         -- bookCount //The amount of books inside a folder
-         -- //Maybe use a foreach loop to simplify drawing the books even further
-         -- newFolder //This is the div that books from the current folder should be appended into
-        */
     }
 }
+/*
+    Temporary Documentation for me for later
+    -----------------------------------------
+    --- folderName //The name/property of the folders object
+    -- folderBooks //The array that stores all the books that go in a folder
+    -- bookCount //The amount of books inside a folder
+    -- //Maybe use a foreach loop to simplify drawing the books even further
+    -- newFolder //This is the div that books from the current folder should be appended into
+*/
 draw(); 
 
 let allFolders = document.getElementsByClassName("folder");
-
-// // Example function for draw folder links
-// function drawFolderLinks() {
-//     let folderIDs = [];
-//     for (let i = 0; i < allFolders.length; i++) {
-//         folderIDs.push(allFolders[i].id);
-//     };
-//     for (let i = 0; i < folderIDs.length; i++) {
-//         //Count number of books in each folder
-//         let countNumber = Array.from(allFolders[i].children).filter(child => child.classList.contains("book")).length
-
-//         //Create labels
-//         let p = document.createElement("p");
-//         p.classList.add("folderLink");
-//         p.innerHTML = `${folderIDs[i]} <i class="fas fa-folder"></i> <span class="folderCount">${countNumber}</span>`;
-//         folderContainer.append(p);
-//     }
-// };
-// // drawFolderLinks();
 
 //Add functionality to folder links REFACTOR INTO DRAW FUNCTION
 for (let i = 0; i < allFolderLinks.length; i++) {
