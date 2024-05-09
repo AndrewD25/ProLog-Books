c3406fd55abe2bfe85bb7aeee7647ebc642fad7b
Andrew Deal <111801931+AndrewD25@users.noreply.github.com>
2024-05-09 14:14:59 -0700

    5/9

--- Scripts/social.js
+++ Scripts/social.js
@@ -13,13 +13,31 @@ function search() {
     let childrenOfUB = userBox.children;
     for (let i = 0; i < childrenOfUB.length; i++) {
         let my = childrenOfUB[i];
-        if (my.textContent.includes(searchBox.value) || my.id === "search") {
+        if (my.textContent.toLowerCase().includes(searchBox.value.toLowerCase()) || my.id === "search") {
             my.classList.remove("hidden");
         } else {
             my.classList.add("hidden");
         }
     }
 };
+
+// Scroll to top
+let mybutton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
 
 searchBox.addEventListener("keyup", search);
 searchBox.addEventListener("click", search);
