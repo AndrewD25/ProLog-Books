923880a89e16a345d9a310b41c7fe1f6df5a5ace
Andrew Deal <111801931+AndrewD25@users.noreply.github.com>
2024-05-15 12:03:44 -0700

    5/15
    Updated index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ DUE DATE
     <head>
         <title>ProLog Books: Organize and Share Your Books Online</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="google-site-verification" content="0W5njWoJhg_KH3laIOv39L5m0Q3PHw0HjPTgyGBtItQ" />
         <link rel="icon" href="Images/PLBLogo.png">
 
         <link rel="stylesheet" href="Stylesheets/pageTemplate.css?v=<?php echo time(); ?>">
@@ -57,7 +58,7 @@ DUE DATE
                     If this is your first time on ProLog books, start here! We require you to create an account and sign in to use some of our features in order to avoid bot spam
                     and keep bandwidth down to create the best experience for you. Don't worry, it's completely free to sign up! Head to our <a href="Pages/signUp.php">Sign Up</a>
                     page to get started. Then check out everything we have to offer at ProLog Books! Not ready to do that yet or already done? Then you should check out our article at
-                    <a href="Pages/article.php?id=1">ProLog Books News</a> on how to get started with our site! Or check out our <a target="_blank" href="userman.pdf">User Manual</a>! 
+                    <a href="Pages/news.php">ProLog Books News</a> on how to get started with our site! Or check out our <a target="_blank" href="UserManual.pdf">User Manual</a>! 
                     We hope you enjoy seeing all we have to offer!
                 </p>
             </div>
@@ -66,17 +67,17 @@ DUE DATE
         <div class="info rightBox">
             <div class="infoText">
                 <h2>Log Your Collection!</h2>
-                <p>On the <a href="Pages/construction.php">Collection</a> page, keep an inventory of all your books. The books stay in the order you add them so you 
+                <p>On the <a href="Pages/collection.php">Collection</a> page, keep an inventory of all your books. The books stay in the order you add them so you 
                 can use it to keep track of reading order. Store titles, publishers, notes, ratings, and any other information you want!    </p>
             </div>
             <div class="previewImage"><img src="Images/collectionPagePreview.jpg?v=<?php echo time(); ?>"></div>
         </div>
 
         <div class="info leftBox">
-            <div class="previewImage"><img src="Images/unfinishedPreview.jpg?v=<?php echo time(); ?>"></div>
+            <div class="previewImage"><img src="Images/socialPreview.jpg?v=<?php echo time(); ?>"></div>
             <div class="infoText">
                 <h2>Share Your Shelf!</h2>
-                <p>Our <a href="Pages/construction.php">Social</a> page is the best place to connect with other book lovers! Create a profile that 
+                <p>Our <a href="Pages/social.php">Social</a> page is the best place to connect with other book lovers! Create a profile that 
                 shows off your favorite parts of your collection and share it with others! Then browse the explore section to see featured 
                 users for the week. Everyone is welcome to express their personality!</p>
             </div>
@@ -104,6 +105,18 @@ DUE DATE
                     all the cool covers and fun new things to do on Comicle!
                 </p>
             </div>
+        </div>
+
+        <div class="info rightBox">
+            <div class="infoText">
+                <h2>And More!</h2>
+                <p>
+                There's always new things to do coming out at ProLog Books! Stay connected with us (<a href="https://www.instagram.com/prologbooks/">@ProLogBooks</a> 
+                on Instagram) for the latest news about the site! Check out our Ko-Fi page (<a href="https://ko-fi.com/prologbooks/">ProLog Books</a>) 
+                if you want to donate to support us and get new features added sooner!
+                </p>
+            </div>
+            <div class="previewImage"><img src="Images/kofiPreview.jpg?v=<?php echo time(); ?>"></div>
         </div>
 
         <script src="Scripts/templateScript.js?v=<?php echo time(); ?>"></script>
